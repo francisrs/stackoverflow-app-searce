5e4098d55dc9ea337ac13810b0a44b67387d9e00
Selva logesh R <selvalogesh95@gmail.com>
2021-09-27 10:02:34 +0530

    Deployment commit

---
+++ precache-manifest.c4181b7f39bcfa8f547799db33389f0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7909b19693a404ccdbd14c6749e6d079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d26422bdcef7b495ff3",
+    "url": "/static/css/main.067863fd.chunk.css"
+  },
+  {
+    "revision": "1b953ad0ead672f2ec6a",
+    "url": "/static/js/2.c7ef109d.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/static/js/2.c7ef109d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d26422bdcef7b495ff3",
+    "url": "/static/js/main.702c770b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0d2819dbeea8f5bc1d960ddbb54fc870",
+    "url": "/static/media/LogoGlyphMd.0d2819db.svg"
+  },
+  {
+    "revision": "77e8ffed3ea33dc90b4c3a9498f14e53",
+    "url": "/static/media/LogoMd.77e8ffed.svg"
+  },
+  {
+    "revision": "acd7c5e8df81d6c020b0f0904e0265a8",
+    "url": "/static/media/PageSpinner.acd7c5e8.svg"
+  },
+  {
+    "revision": "fdbe4a204c2da73c5e5603b45567939d",
+    "url": "/static/media/Search.fdbe4a20.svg"
+  },
+  {
+    "revision": "f5667a3b68d176bcf004c2a419d3bca7",
+    "url": "/static/media/three-dots.f5667a3b.svg"
+  }
+]);
